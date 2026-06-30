16429332027a7cecee90d907bfd9658be68c9220
DentesdeSabre <dentesdesabre100@gmail.com>
2026-06-30 13:01:11 -0300

    Delete app directory

--- app/api/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export async function POST(req: NextRequest) {
-  const res = NextResponse.json({ ok: true })
-  res.cookies.set({ name: 'karacol_admin', value: '', httpOnly: true, path: '/', maxAge: 0 })
-  return res
-}
