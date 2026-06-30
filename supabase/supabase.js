8c81a5f9ce697d51577b752304f6f1ac181d3fd1
DentesdeSabre <dentesdesabre100@gmail.com>
2026-06-30 20:50:18 +0000

    Login

--- supabase/supabase.js
+++ supabase/supabase.js
@@ -3,7 +3,14 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient('https://mqgfubesmocuwtobucgo.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xZ2Z1YmVzbW9jdXd0b2J1Y2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MjU3MTMsImV4cCI6MjA5ODQwMTcxM30.A1zTveUT3bsWB1n5m0oUgIGVyIr_PA6o-7zDroduKvQ')
 
+const { data, error } = await supabase.auth.signInWithPassword({
+            email: 'dentesdesabre100@gmail.com',
+            password: 'Reino1914#',
+        });
 
+        if (error) {
+            alert("Erro ao logar: " + error.message);
+        }
 
 document.getElementById('enviar').addEventListener("click",enviar);
 document.getElementById('confDel').addEventListener("click",deletar);
